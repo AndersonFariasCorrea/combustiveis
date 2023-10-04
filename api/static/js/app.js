d3b82a5aa8b1457e54c083c6b858d98295702954
Anderson Correa <deidaraa2002@gmail.com>
2023-10-04 17:58:48 -0300

    trying to add the graphics

--- api/static/js/app.js
+++ api/static/js/app.js
@@ -7,5 +7,6 @@ app.controller('myCtrl', function($scope) {
     $scope.tipos_combustiveis = ['disel', 'gás', 'gasolina'];
     $scope.estados = ["Acre", "Alagoas", "Amapá", "Amazonas", "Bahia", "Ceará", "Distrito Federal", "Espírito Santo", "Goiás", "Maranhão", "Mato Grosso", "Mato Grosso do Sul", "Minas Gerais", "Pará", "Paraíba", "Paraná", "Pernambuco", "Piauí", "Rio de Janeiro", "Rio Grande do Norte", "Rio Grande do Sul", "Rondônia", "Roraima", "Santa Catarina", "São Paulo", "Sergipe", "Tocantins"];
     $scope.municipios = ["Anapolis", "Goiânia", "Aparecida de Goiânia"];
-    $scope.bairros = [];
+    $scope.bairros = ["Morada do Sol", "Centro", "Centro Oeste", "Centro Sul"];
+    $scope.medias = [{ano: 2014, cidade: "Goiânia", tipo: "gasolina", média_compra: 3.22, media_venda: 4.30}, {ano: 2014, cidade: "Aparecida Goiânia", tipo: "gasolina", média_compra: 3.52, media_venda: 4.90}]
 });
