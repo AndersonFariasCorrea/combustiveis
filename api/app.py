681dbdbacd8ab398bcaa718afd2f28a0386fa79b
andersonFariasCorrea <deidaraa2002@gmail.com>
2023-10-06 06:04:19 -0300

    mixed and deleted some staff

--- api/app.py
+++ api/app.py
@@ -1,39 +1,51 @@
-from flask import Flask, render_template, request, jsonify
-from core.db_conn import db_config  # Import the database configuration from core/db_conn.py
+from flask import Flask, request, jsonify, render_template
+import pandas as pd
+# Import the database configuration from core/db_conn.py
 # import mysql.connector
 
 app = Flask(__name__)
 
-# conn = mysql.connector.connect(**db_config)
-# cursor = conn.cursor()
+# Load your data into a global DataFrame
+df = pd.read_csv("combustiveis.csv")
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-# @app.route("/api", methods=['GET'])
-# def get_combustiveis():
-#     # Get query parameters from the request
-#     ano = request.args.get('ano', type=int)
-#     limit = request.args.get('limit', type=int)
+@app.route("/api", methods=['GET'])
+def get_combustiveis():
+    # Get query parameters from the request
+    ano = request.args.get('ano', type=int)
+    produto = request.args.get('produto', type=str)
+    limit = request.args.get('limit', type=int)
 
-#     if ano is None:
-#         return jsonify({'status': 400, 'message': 'Invalid parameters'}), 400
+    if ano is None or produto is None:
+        return jsonify({'status': 400, 'message': 'Invalid parameters'}), 400
 
-#     try:
-#         query = "SELECT preco_venda, sigla_uf FROM combustiveis WHERE ano = %s LIMIT %s"
-#         cursor.execute(query, (ano, limit))
-#         results = cursor.fetchall()
-#         columns = [column[0] for column in cursor.description]
-#         data = [dict(zip(columns, row)) for row in results]
+    pd.options.display.float_format = "{:,.2f}".format
 
-#         if not data:
-#             return jsonify({'status': 404, 'message': 'No data found'}), 404
+    # Filter the DataFrame
+    filtered_df = df.loc[(df['ano'] == ano) & (df['produto'] == produto), ['ano', 'produto', 'preco_venda', 'sigla_uf']]
 
-#         return jsonify(data)
+    if filtered_df.empty:
+        return jsonify({'status': 404, 'message': 'No data found'}), 404
 
-#     except mysql.connector.Error as error:
-#         return jsonify({'error': str(error)}), 500
+    # Optionally, limit the number of rows if 'limit' is provided
+    if limit:
+        filtered_df = filtered_df.head(limit)
+
+    # Convert the filtered DataFrame to a list of dictionaries and jsonify it
+    result_list = filtered_df.to_dict(orient='records')
+    return jsonify(result_list)
 
 if __name__ == '__main__':
     app.run()
+
+# FORMAT PLUS SAMPLE FROM TABLE
+# | ano  | sigla_uf | id_municipio |   bairro_revenda  | cep_revenda|    endereco_revenda   |   cnpj_revenda |   nome_estabelecimento  |
+# |------|----------|--------------|-------------------|------------|----------------------------------------|-------------------------|
+# | 2009 |   SP     |   3530508    |   Vila Lambari    | 13731087   | Praca Capitão Olimpio | 04363381000124 | Auto Posto Ebenezer Ltda|
+
+# | bandeira_revenda | data_coleta | produto | unidade_medida | preco_compra | preco_venda |
+# |------------------|-------------|---------|----------------|--------------|-------------|
+# |     Branca       | 2009-07-06  | Diesel  |  R$/litro      |    -         |   1.999     |
--- api/static/js/app.js
+++ api/static/js/app.js
@@ -1,12 +1,61 @@
 var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope) {
-    $(document).ready(() => {
-        
-    });
     $scope.years = [2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023];
     $scope.tipos_combustiveis = ['disel', 'gás', 'gasolina'];
-    $scope.estados = ["Acre", "Alagoas", "Amapá", "Amazonas", "Bahia", "Ceará", "Distrito Federal", "Espírito Santo", "Goiás", "Maranhão", "Mato Grosso", "Mato Grosso do Sul", "Minas Gerais", "Pará", "Paraíba", "Paraná", "Pernambuco", "Piauí", "Rio de Janeiro", "Rio Grande do Norte", "Rio Grande do Sul", "Rondônia", "Roraima", "Santa Catarina", "São Paulo", "Sergipe", "Tocantins"];
-    $scope.municipios = ["Anapolis", "Goiânia", "Aparecida de Goiânia"];
+    $scope.estados = [
+        { nome: 'Acre', sigla: 'AC' },
+        { nome: 'Alagoas', sigla: 'AL' },
+        { nome: 'Amapá', sigla: 'AP' },
+        { nome: 'Amazonas', sigla: 'AM' },
+        { nome: 'Bahia', sigla: 'BA' },
+        { nome: 'Ceará', sigla: 'CE' },
+        { nome: 'Distrito Federal', sigla: 'DF' },
+        { nome: 'Espírito Santo', sigla: 'ES' },
+        { nome: 'Goiás', sigla: 'GO' },
+        { nome: 'Maranhão', sigla: 'MA' },
+        { nome: 'Mato Grosso', sigla: 'MT' },
+        { nome: 'Mato Grosso do Sul', sigla: 'MS' },
+        { nome: 'Minas Gerais', sigla: 'MG' },
+        { nome: 'Pará', sigla: 'PA' },
+        { nome: 'Paraíba', sigla: 'PB' },
+        { nome: 'Paraná', sigla: 'PR' },
+        { nome: 'Pernambuco', sigla: 'PE' },
+        { nome: 'Piauí', sigla: 'PI' },
+        { nome: 'Rio de Janeiro', sigla: 'RJ' },
+        { nome: 'Rio Grande do Norte', sigla: 'RN' },
+        { nome: 'Rio Grande do Sul', sigla: 'RS' },
+        { nome: 'Rondônia', sigla: 'RO' },
+        { nome: 'Roraima', sigla: 'RR' },
+        { nome: 'Santa Catarina', sigla: 'SC' },
+        { nome: 'São Paulo', sigla: 'SP' },
+        { nome: 'Sergipe', sigla: 'SE' },
+        { nome: 'Tocantins', sigla: 'TO' }
+    ];
     $scope.bairros = ["Morada do Sol", "Centro", "Centro Oeste", "Centro Sul"];
-    $scope.medias = [{ano: 2014, cidade: "Goiânia", tipo: "gasolina", média_compra: 3.22, media_venda: 4.30}, {ano: 2014, cidade: "Aparecida Goiânia", tipo: "gasolina", média_compra: 3.52, media_venda: 4.90}]
+    $scope.medias = [{ano: 2014, cidade: "Goiânia", tipo: "gasolina", média_compra: 3.22, media_venda: 4.30}, {ano: 2014, cidade: "Aparecida Goiânia", tipo: "gasolina", média_compra: 3.52, media_venda: 4.90}];
+
+    $scope.yearIsGreaterFilter = function(year) {
+        if (typeof $scope.combustivel?.yearFrom !== 'undefined') return year > $scope.combustivel.yearFrom;
+    };
+
+    const ctx = document.getElementById('myChart');
+
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: $scope.medias.filter(item => item.cidade).map(item => item.cidade),
+            datasets: [{
+            label: '# Média de compra',
+            data: $scope.medias.filter(item => item.média_compra).map(item => item.média_compra),
+            borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+            y: {
+                beginAtZero: true
+            }
+            }
+        }
+    });
 });
